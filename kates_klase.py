522704c8cab127c3ad188ad8a06360e598e4621a
Heka <code@epixel.lt>
2023-08-18 12:28:55 +0300

    __repr__ ir papildyti requirements

--- kates_klase.py
+++ kates_klase.py
@@ -11,3 +11,6 @@ class Kate:
     def __str__(self):
         # return f'Hello, mes pirmą kartą pakeitėme ką Python printins' (pakeis veiškų printą į mūsų norimą stringą
         return f"Hello, mano vardas {self.vardas}, spalva {self.spalva}"
+
+    def __repr__(self):  # Magic metodas , kad spausdintų listuose
+        return f" {self.vardas}|{self.spalva}|{self.kojos}"
