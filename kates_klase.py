5bb5390cfa4743876e1d0fbfc0a6a37da90902b3
Heka <code@epixel.lt>
2023-08-18 13:15:14 +0300

    Katės kalsės printai su nauju failu laikinas.py

--- kates_klase.py
+++ kates_klase.py
@@ -14,3 +14,32 @@ class Kate:
 
     def __repr__(self):  # Magic metodas , kad spausdintų listuose
         return f" {self.vardas}|{self.spalva}|{self.kojos}"
+
+
+kate1_obj = Kate('Juoda')
+print(kate1_obj)
+
+print(kate1_obj.spalva)
+kate1_obj.spalva = 'raina'
+print(kate1_obj)
+
+kate2_obj = Kate("balta", vardas="Albinosė")
+print(kate2_obj)
+
+kate3_obj = Kate("ruda", vardas="Rudė")
+print(kate3_obj)
+kate3_obj.vardas = "Ryžė"
+print(kate3_obj)
+
+kaciu_listas = [kate1_obj, kate2_obj, kate3_obj]  # sudedam į listą kates
+print(kaciu_listas)
+
+print('--------------')
+
+for kate_obj in kaciu_listas:
+    #  print(kate_obj.vardas, kate_obj.spalva)
+    if kate_obj.spalva == 'balta':
+        print('Baltos kaitės vardas', kate_obj.vardas)
+        kate_obj.vardas = 'Meška'
+
+print(kaciu_listas)
---
+++ laikinas.py
@@ -0,0 +1,9 @@
+"""
+Failas laikinas.py yra tiesiog Python skriptas,
+taigi jame galima aprašyti ir vykdyti bet kokį Python kodą.
+Jūs galite kurti kintamuosius, funkcijas, klases, naudoti įvairias bibliotekas,
+vykdyti skaičiavimus, dirbti su failais, ir daug daugiau.
+
+Pavyzdžiui, galite atidaryti laikinas.py failą savo mėgstamame teksto redaktoriuje
+arba kodo redaktoriuje, parašyti savo Python kodą ir jį vykdyti.
+"""
